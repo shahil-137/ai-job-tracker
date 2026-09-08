f8e03880091ceda06c631e0f1245fc083456ab35
shahil-137 <mshahil137@gmail.com>
2026-09-08 22:58:02 +0530

    Configure allowed hosts for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ai-job-tracker-4-ilsp.onrender.com",
+    "localhost",
+    "127.0.0.1",]
 
 
 # Application definition
