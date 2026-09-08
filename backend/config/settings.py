21eca114131a27e17792a02ddee7597a0a8b850f
shahil-137 <mshahil137@gmail.com>
2026-09-08 22:52:57 +0530

    Fix CORS for Vercel frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://ai-job-tracker-4-ilsp.onrender.com"
+     "https://ai-job-tracker-rho-khaki.vercel.app"
 ]
 
 ROOT_URLCONF = 'config.urls'
