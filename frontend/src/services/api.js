d6950fba842115463d68af3e992535cbd2f24f46
shahil-137 <mshahil137@gmail.com>
2026-09-08 23:03:52 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://127.0.0.1:8000/api";
-const API_URL="https://ai-job-tracker-4-ilsp.onrender.com"
+const API_URL="https://ai-job-tracker-4-ilsp.onrender.com/api"
 
 
 export async function login(username, password) {
